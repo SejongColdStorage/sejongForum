2d6cbdf61cdf19093c1576214763001646f18bb8
SejongPark <devsejong@gmail.com>
2014-12-25 22:00:00 +0900

    정적파일 연결고리 추가.

--- src/main/java/kr/sadalmelik/Application.java
+++ src/main/java/kr/sadalmelik/Application.java
@@ -1,29 +1,21 @@
 package kr.sadalmelik;
 
-import com.github.jknack.handlebars.springmvc.HandlebarsViewResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
+
+
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application{
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public ViewResolver handlebarViewResolver() {
-
-        HandlebarsViewResolver viewResolver = new HandlebarsViewResolver();
-        viewResolver.setOrder(1);
-        viewResolver.setPrefix("views/");
-        viewResolver.setSuffix(".hbs");
-
-        return viewResolver;
+        //TODO object배열 단순화 시키기.
+        SpringApplication.run(new Object[]{
+                Application.class,
+                ForumWebMvcConfiguration.class
+        }, args);
     }
 }
---
+++ src/main/java/kr/sadalmelik/ForumWebMvcConfiguration.java
@@ -0,0 +1,47 @@
+package kr.sadalmelik;
+
+import com.github.jknack.handlebars.springmvc.HandlebarsViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ForumWebMvcConfiguration extends WebMvcConfigurerAdapter {
+
+    private final static int CACHE_PERIOD = 31556926;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/not-found").setViewName("common/404");
+        registry.addViewController("/error").setViewName("common/500");
+
+        registry.addViewController("/").setViewName("about");
+        registry.addViewController("/about").setViewName("about");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String[] pathPatterns = new String[]{"/components/**", "/js/**", "/css/**", "/img/**"};
+        String[] locations = new String[]{"assets/components/", "assets/js/", "assets/css", "assets/img/"};
+        int cachePeriod = CACHE_PERIOD;
+
+        registry.addResourceHandler(pathPatterns)
+                .addResourceLocations(locations)
+                .setCachePeriod(cachePeriod);
+    }
+
+    @Bean
+    public ViewResolver handlebarViewResolver() {
+
+        HandlebarsViewResolver viewResolver = new HandlebarsViewResolver();
+        viewResolver.setOrder(1);
+        viewResolver.setPrefix("views/");
+        viewResolver.setSuffix(".hbs");
+
+        return viewResolver;
+    }
+}
