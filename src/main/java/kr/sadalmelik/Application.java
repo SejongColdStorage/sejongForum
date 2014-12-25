012a86be2e26104ba39c6ffc2afdefca1ffc6dc1
SejongPark <devsejong@gmail.com>
2014-12-25 21:20:09 +0900

    handlebars로 템플릿 엔진 변경완료.

--- src/main/java/kr/sadalmelik/Application.java
+++ src/main/java/kr/sadalmelik/Application.java
@@ -1,9 +1,12 @@
 package kr.sadalmelik;
 
+import com.github.jknack.handlebars.springmvc.HandlebarsViewResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 
 @Configuration
 @ComponentScan
@@ -11,5 +14,16 @@ import org.springframework.context.annotation.Configuration;
 public class Application{
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ViewResolver handlebarViewResolver() {
+
+        HandlebarsViewResolver viewResolver = new HandlebarsViewResolver();
+        viewResolver.setOrder(1);
+        viewResolver.setPrefix("views/");
+        viewResolver.setSuffix(".hbs");
+
+        return viewResolver;
     }
 }
--- src/main/java/kr/sadalmelik/contoller/ForumController.java
+++ src/main/java/kr/sadalmelik/contoller/ForumController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class PostController {
-
+public class ForumController {
+    @RequestMapping("/")
+    public String forum() {
+        return "greeting";
+    }
 }
